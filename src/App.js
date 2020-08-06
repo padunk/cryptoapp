216a2e19872968bb3e942934b1f6f969c93bde10
Padunk <abraham.agel@gmail.com>
2020-08-06 17:35:39 +0700

    get /coins to show on front end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
+import { API } from "aws-amplify";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [coins, updateCoins] = React.useState([]);
+
+  async function fetchCoins() {
+    const data = await API.get("cryptoapi", "/coins");
+    updateCoins(data.coins);
+  }
+
+  React.useEffect(() => {
+    fetchCoins();
+  }, []);
+
+  return (
+    <div>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} = {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default hot(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import regeneratorRuntime from "regenerator-runtime";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+
+Amplify.configure(config);
 
 import App from "./App";
 
