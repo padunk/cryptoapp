730a987c495e2ed3c1ae4c077f057a7fe4122737
Padunk <abraham.agel@gmail.com>
2020-08-06 18:12:45 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -5,10 +5,27 @@ import "./App.css";
 
 function App() {
   const [coins, updateCoins] = React.useState([]);
+  const [input, updateInput] = React.useState({ limit: 5, start: 0 });
+  const [fetchStatus, updateFetchStatus] = React.useState("pending"); // pending, success, error
+
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
 
   async function fetchCoins() {
-    const data = await API.get("cryptoapi", "/coins");
-    updateCoins(data.coins);
+    updateFetchStatus("pending");
+
+    const { limit, start } = input;
+    const data = await API.get(
+      "cryptoapi",
+      `/coins?limit=${limit}&start=${start}`
+    );
+    if (data.coins.error) {
+      updateFetchStatus("error");
+    } else {
+      updateFetchStatus("success");
+      updateCoins(data.coins);
+    }
   }
 
   React.useEffect(() => {
@@ -17,14 +34,29 @@ function App() {
 
   return (
     <div>
-      {coins.map((coin, index) => (
-        <div key={index}>
-          <h2>
-            {coin.name} = {coin.symbol}
-          </h2>
-          <h5>${coin.price_usd}</h5>
-        </div>
-      ))}
+      <input
+        onChange={(e) => updateInputValues("limit", e.target.value)}
+        placeholder='limit'
+      />
+      <input
+        placeholder='start'
+        onChange={(e) => updateInputValues("start", e.target.value)}
+      />
+      <button onClick={fetchCoins}>Fetch Coins</button>
+      {fetchStatus === "pending" ? (
+        <div>Loading</div>
+      ) : fetchStatus === "success" ? (
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>
+              {coin.name} = {coin.symbol}
+            </h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      ) : (
+        <div>Error</div>
+      )}
     </div>
   );
 }
